575e53b83adc382ce016c8ce5a4483607605b344
Luigi Greco <9luigi1@gmail.com>
2021-07-29 21:12:41 +0200

    Added new objects, fixed animation, cleaned up unused code

--- WristBuddy WatchKit Extension/BuddyScene.swift
+++ WristBuddy WatchKit Extension/BuddyScene.swift
@@ -15,21 +15,51 @@ class BuddyScene: SKScene {
     var lionRestingFrames: [SKTexture] = []
     var lionWavingFrames: [SKTexture] = []
     var firstFrameTexture = SKTexture()
+    var boneNode = SKSpriteNode()
+    var soapNode = SKSpriteNode()
+    var ballNode = SKSpriteNode()
+    let objectsAtlas = SKTextureAtlas(named: "objects")
     
     override func sceneDidLoad() {
         setupRestingAnimation()
         restLion()
         scene?.addChild(lion)
-        
+        setupBone()
+        setupSoap()
+        setupBall()
+    }
+    
+    func setupBone(){
+        let boneFrame = objectsAtlas.textureNamed("bone")
+        let setBone = SKAction.setTexture(boneFrame, resize: false)
+        boneNode.run(setBone)
+        boneNode.position = CGPoint(x: frame.midX - 50 , y: frame.midY - 80)
+        scene?.addChild(boneNode)
+    }
+    
+    func setupSoap(){
+        let soapFrame = objectsAtlas.textureNamed("soap")
+        let setSoap = SKAction.setTexture(soapFrame, resize: false)
+        soapNode.run(setSoap)
+        soapNode.position = CGPoint(x: frame.midX , y: frame.midY - 80)
+        soapNode.setScale(0.5)
+        scene?.addChild(soapNode)
+    }
+    
+    func setupBall(){
+        let ballFrame = objectsAtlas.textureNamed("ball")
+        let setBall = SKAction.setTexture(ballFrame, resize: false)
+        ballNode.run(setBall)
+        ballNode.position = CGPoint(x: frame.midX + 50, y: frame.midY - 80)
+        scene?.addChild(ballNode)
     }
     
     func setupRestingAnimation(){
         lion.removeAllActions()
-        let lionRestingAtlas = SKTextureAtlas(named: "lionResting")
+        let lionRestingAtlas = SKTextureAtlas(named: "lionWaving")
         var restFrames: [SKTexture] = []
-        let numImages = lionRestingAtlas.textureNames.count
-        for i in 1...numImages {
-            let lionTextureName = "lionResting\(i)"
+        for i in 1...3 {
+            let lionTextureName = "lionWaving\(i)"
             restFrames.append(lionRestingAtlas.textureNamed(lionTextureName))
         }
         lionRestingFrames = restFrames
@@ -40,17 +70,17 @@ class BuddyScene: SKScene {
     }
     
     func restLion(){
+        
         setupRestingAnimation()
-        print("Resting...")
         lion.run(SKAction.repeatForever(SKAction.animate(with: lionRestingFrames, timePerFrame: 0.3, resize: false, restore: false)), withKey: "restingLion")
+        
     }
     func waveLion(){
-        print("Hello!")
         lion.removeAllActions()
         let lionWavingAtlas = SKTextureAtlas(named: "lionWaving")
         var waveFrames: [SKTexture] = []
         let numImages = lionWavingAtlas.textureNames.count
-        for i in 1...numImages {
+        for i in 4...numImages {
             let lionTextureName = "lionWaving\(i)"
             waveFrames.append(lionWavingAtlas.textureNamed(lionTextureName))
         }
@@ -63,19 +93,22 @@ class BuddyScene: SKScene {
         firstFrameTexture = lionWavingFrames[0]
         let setWaveTexture = SKAction.setTexture(firstFrameTexture, resize: false)
         lion.run(setWaveTexture)
-        lion.run(SKAction.repeat(SKAction.animate(with: lionWavingFrames, timePerFrame: 0.3, resize: false, restore: true), count: 1), completion: {
+        lion.run(SKAction.repeat(SKAction.animate(with: lionWavingFrames, timePerFrame: 0.2, resize: false, restore: false), count: 1), completion: {
             self.restLion()
         })
-        
 
     }
     
     func didTap(atPosition: CGPoint) {
-        print("label at \(labelNode.position)")
-        print("sprite at \(testNode.position)")
         let hitNodes = self.nodes(at: atPosition)
         if hitNodes.contains(lion) {
             waveLion()
+        } else if hitNodes.contains(boneNode) {
+            print("Gave bone")
+        } else if hitNodes.contains(soapNode) {
+            print("Washing!")
+        } else if hitNodes.contains(ballNode) {
+            print("Let's play")
         }
         
     }
--- WristBuddy WatchKit Extension/InterfaceController.swift
+++ WristBuddy WatchKit Extension/InterfaceController.swift
@@ -31,16 +31,9 @@ class InterfaceController: WKInterfaceController {
 
     
     @IBAction func handleTap(tapGestureRecognizer: WKTapGestureRecognizer){
-        let location = tapGestureRecognizer.locationInObject()
-        let screenSize = WKInterfaceDevice.current().screenBounds
-        print("width \(scene!.size.width)")
-        print("height \(scene!.size.height)")
         let tapX = (tapGestureRecognizer.locationInObject().x - (scene!.size.width / 2))
         let tapY = ((scene!.size.height / 2) -  tapGestureRecognizer.locationInObject().y   )
         let tap = CGPoint(x: tapX, y: tapY)
-        print("Tapped at \(tap)")
-        //let anchorLocation = CGPoint(x: location.x - (screenSize.width)/2, y: location.y - (screenSize.height)/2)
-        //print("tapped at \(anchorLocation)")
         WKInterfaceDevice.current().play(.click)
         scene?.didTap(atPosition: tap)
     }
