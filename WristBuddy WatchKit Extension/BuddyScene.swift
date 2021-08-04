a7aae5df85d3613b1989e707f95033649b2d4691
Luigi Greco <9luigi1@gmail.com>
2021-08-04 19:03:03 +0200

    Fixed crashing when tapping before animation ended; added ball playing animation

--- WristBuddy WatchKit Extension/BuddyScene.swift
+++ WristBuddy WatchKit Extension/BuddyScene.swift
@@ -17,16 +17,21 @@ class BuddyScene: SKScene {
     var dogBathFrames: [SKTexture] = []
     var dogBoneEatingFrames: [SKTexture] = []
     var boneAnimationFrames: [SKTexture] = []
+    var bubblesAnimationFrames: [SKTexture] = []
     var ballAnimationFrames: [SKTexture] = []
+    var ballPlayingFrames: [SKTexture] = []
     var firstFrameTexture = SKTexture()
     var boneNode = SKSpriteNode()
     var soapNode = SKSpriteNode()
     var ballNode = SKSpriteNode()
     var boneAnimNode = SKSpriteNode()
+    var bubblesNode = SKSpriteNode()
     var ballAnimNode = SKSpriteNode()
     let objectsAtlas = SKTextureAtlas(named: "objects")
+    let bubblesAtlas = SKTextureAtlas(named: "bubbles")
     
     override func sceneDidLoad() {
+        bubblesNode.name = "bubblesNode"
         setupRestingAnimation()
         restDog()
         scene?.addChild(dog)
@@ -44,6 +49,9 @@ class BuddyScene: SKScene {
     }
     
     func setupBoneAnim(){
+        if (childNode(withName: "boneAnimNode") as! SKSpriteNode?) != nil {
+            print("Node is already present, wait for animation to finish")
+        } else {
         let boneAnimAtlas = SKTextureAtlas(named: "boneAnim")
         var boneAnimFrames: [SKTexture] = []
         let numImages = boneAnimAtlas.textureNames.count
@@ -58,10 +66,54 @@ class BuddyScene: SKScene {
         boneAnimNode.setScale(1.4)
         boneAnimNode.position = CGPoint(x: frame.midX, y: frame.midY + 20)
         scene?.addChild(boneAnimNode)
+        boneAnimNode.name = "boneAnimNode"
         boneAnimNode.run(SKAction.animate(with: boneAnimationFrames, timePerFrame: 0.15, resize: false, restore: false), completion: boneEatingAnim)
+    }
+    }
+
+    func setupBubblesAnim1(){
+        if (childNode(withName: "bubblesNode") as! SKSpriteNode?) != nil {
+            print("Node is already present, wait for animation to finish")
+        } else {
+        var bubblesFrames: [SKTexture] = []
+        for i in 1...6 {
+            let bubblesTextureName = "bubbles\(i)"
+            bubblesFrames.append(bubblesAtlas.textureNamed(bubblesTextureName))
+        }
+        //bubblesAnimationFrames = bubblesFrames
+        firstFrameTexture = bubblesFrames[0]
+        let setAnimTexture = SKAction.setTexture(firstFrameTexture, resize: false)
+        bubblesNode.run(setAnimTexture)
+        bubblesNode.setScale(1.5)
+        scene?.addChild(bubblesNode)
+        bubblesNode.name = "bubblesNode"
+        bubblesNode.run(SKAction.animate(with: bubblesFrames, timePerFrame: 0.15, resize: false, restore: false), completion: setupBubblesAnim2)
+        
+        }
+    }
+    
+    func setupBubblesAnim2(){
+        dog.removeAllActions()
+        let dogBathAtlas = SKTextureAtlas(named: "dogBath")
+        let bathFrame = dogBathAtlas.textureNamed("dogBath1")
+        let setBathTexture = SKAction.setTexture(bathFrame, resize: false)
+        dog.run(setBathTexture)
+        var bubblesFrames: [SKTexture] = []
+        let numImages = bubblesAtlas.textureNames.count
+        for i in 7...numImages {
+            let bubblesTextureName = "bubbles\(i)"
+            bubblesFrames.append(bubblesAtlas.textureNamed(bubblesTextureName))
+        }
+        firstFrameTexture = bubblesFrames[0]
+        let setAnimTexture = SKAction.setTexture(firstFrameTexture, resize: false)
+        bubblesNode.run(setAnimTexture)
+        bubblesNode.run(SKAction.animate(with: bubblesFrames, timePerFrame: 0.15, resize: false, restore: false), completion: bathDog)
     }
     
     func setupBallAnim(){
+        if (childNode(withName: "ballAnimNode") as! SKSpriteNode?) != nil {
+            print("Node is already present, wait for animation to finish")
+        } else {
         let ballAnimAtlas = SKTextureAtlas(named: "ballAnim")
         var ballAnimFrames: [SKTexture] = []
         let numImages = ballAnimAtlas.textureNames.count
@@ -73,9 +125,26 @@ class BuddyScene: SKScene {
         firstFrameTexture = ballAnimationFrames[0]
         let setAnimTexture = SKAction.setTexture(firstFrameTexture, resize: false)
         ballAnimNode.run(setAnimTexture)
-        ballAnimNode.position = CGPoint(x: frame.midX, y: frame.midY + 20)
+        ballAnimNode.position = CGPoint(x: frame.midX, y: frame.midY - 40)
         scene?.addChild(ballAnimNode)
-        ballAnimNode.run(SKAction.animate(with: ballAnimationFrames, timePerFrame: 0.15, resize: false, restore: false), completion: ballAnimNode.removeFromParent)
+        ballAnimNode.name = "ballAnimNode"
+        ballAnimNode.run(SKAction.animate(with: ballAnimationFrames, timePerFrame: 0.15, resize: false, restore: false), completion: ballAnim)
+    }
+    }
+    
+    func ballAnim(){
+        ballAnimNode.removeFromParent()
+        let ballPlayingAtlas = SKTextureAtlas(named: "ballPlaying")
+        let numImages = ballPlayingAtlas.textureNames.count
+        for i in 1...numImages {
+            let ballPlayingTextureName = "ballPlaying\(i)"
+            ballPlayingFrames.append(ballPlayingAtlas.textureNamed(ballPlayingTextureName))
+        }
+        firstFrameTexture = ballPlayingFrames[0]
+        let setBallAnimTexture = SKAction.setTexture(firstFrameTexture, resize: false)
+        dog.run(setBallAnimTexture)
+        dog.run(SKAction.animate(with: ballPlayingFrames, timePerFrame: 0.15, resize: false, restore: true))
+        
     }
     
     func boneEatingAnim() {
@@ -160,6 +229,7 @@ class BuddyScene: SKScene {
     }
     
     func bathDog(){
+        bubblesNode.removeFromParent()
         dog.removeAllActions()
         let dogBathAtlas = SKTextureAtlas(named: "dogBath")
         var bathFrames: [SKTexture] = []
@@ -185,7 +255,7 @@ class BuddyScene: SKScene {
         } else if hitNodes.contains(boneNode) {
             setupBoneAnim()
         } else if hitNodes.contains(soapNode) {
-            bathDog()
+            setupBubblesAnim1()
         } else if hitNodes.contains(ballNode) {
             setupBallAnim()
         }
